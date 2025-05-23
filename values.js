a3ad9e45a103591cd75179f539f2c04a13a6bdb6
Yefimchuk08 <dmytroyefimchuk08@gmail.com>
2025-05-23 22:56:15 +0300

    Create values.js

---
+++ values.js
@@ -0,0 +1,73 @@
+//Task 1
+// let name = prompt("Enter your name");
+// alert(`Hello ${name}`);
+
+//Task 2
+// const year = 2025;
+// const const_month = 5;
+// let age = parseInt(prompt("Enter your birth year"))
+// let month = parseInt(prompt("Enter your birth month"))
+// if(month < const_month || month===const_month)
+// {
+//     alert(`Your age: ${year-age}`)
+// }
+// else
+// {
+//     alert(`Your age: ${year-age-1}`)
+// }
+
+//Task 3
+// let a = parseInt(prompt("Enter sizew of square"))
+// alert(`P(square): ${a*4}`)
+
+//Task 4
+// let r = parseInt(prompt("Enter radius of circle"))
+// const pi = 3.14
+// alert(`S(circle): ${r**2*pi}`)
+
+//Task 5
+// let way = parseInt(prompt("Enter way"))
+// let time = parseFloat(prompt("Enter time"))
+// alert(`Your speed should be: ${way/time}`)
+
+//Task 6
+// const convert = 0.89
+// let dollar = parseFloat(prompt("Enter money"))
+// alert(`Your cash in euro: ${dollar*convert}`)
+
+//Task 7
+// let volume = parseFloat(prompt("Enter volume in flesh"))
+// let resultInMB = volume*1024
+// const file = 820
+// alert(`You can upload: ${resultInMB/file} files`)
+
+//Task 8
+// let price = parseInt(prompt("Enter chocolate price"))
+// let cash = parseInt(prompt("Enter your cash"))
+// let count = 0
+// while(cash>price)
+// {
+//     cash-=price
+//     count++
+// }
+// alert(`You can buy ${count} chocolates and in the end you have ${cash} money`)
+
+//Task 9
+// let num = parseFloat(prompt("Enter number"))
+// let reverse = 0;
+// let currentNum
+// while(num>0)
+// {
+//     currentNum = num%10
+//     reverse=reverse*10+currentNum
+
+// }
+// alert(reverse)
+
+//Task 10
+// let num = parseInt(prompt("Enter num"));
+
+// console.log(
+//   (num % 2 === 0 && "Parne") || "Neparne"
+// );
+
